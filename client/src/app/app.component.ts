eed9be5a210b73a0501afe6417c6710e03974df9
VI Nguyen <nguyenhungvicb@gmail.com>
2022-03-24 17:33:01 +0700

    dynamic padding top for main

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = "client";
+  className = "w-[95%] sm:w-11/12 md:w-4/5 m-auto pt-36";
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((e) => {
+        const event = e as NavigationEnd;
+        this.checkRouter(event.url);
+      });
+  }
+  checkRouter(url: string) {
+    if (!url.includes("discover") || url !== "/" || !url.includes("browse")) {
+      this.className = "w-[95%] sm:w-11/12 md:w-4/5 m-auto pt-14";
+    }
+  }
 }
