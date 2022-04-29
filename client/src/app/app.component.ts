da61021eb489cd3208afe17fe452e448499dbdca
VI Nguyen <nguyenhungvicb@gmail.com>
2022-04-29 09:23:01 +0700

    Cart ui and functionalities

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { NavigationEnd, Router } from "@angular/router";
-import { filter } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -10,25 +8,6 @@ import { filter } from "rxjs";
 export class AppComponent implements OnInit {
   title = "client";
   className = "";
-  constructor(private router: Router) {}
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((e) => {
-        const event = e as NavigationEnd;
-        this.checkRouter(event.url);
-      });
-  }
-  checkRouter(url: string) {
-    if (
-      !url.includes("discover") &&
-      url !== "/" &&
-      !url.includes("browse") &&
-      !url.includes("detail")
-    ) {
-      this.className = "w-[95%] sm:w-11/12 md:w-4/5 m-auto pt-14";
-    } else {
-      this.className = "w-[95%] sm:w-11/12 md:w-4/5 m-auto pt-36";
-    }
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
--- client/src/app/cart/cart.module.ts
+++ client/src/app/cart/cart.module.ts
@@ -4,18 +4,19 @@ import { RouterModule } from "@angular/router";
 import { CartDetailComponent } from "./cart-detail/cart-detail.component";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
-import { CartItemComponent } from './cart-detail/cart-item/cart-item.component';
+import { CartItemComponent } from "./cart-detail/cart-item/cart-item.component";
+import { FormsModule } from "@angular/forms";
+import { LoadingPipe } from "../loading.pipe";
 
 @NgModule({
-  declarations: [
-    CartDetailComponent,
-    CartItemComponent
-  ],
+  declarations: [CartDetailComponent, CartItemComponent, LoadingPipe],
   imports: [
     CommonModule,
     MatIconModule,
     MatButtonModule,
+    FormsModule,
     RouterModule.forChild([{ path: "cart", component: CartDetailComponent }]),
   ],
+  providers: [LoadingPipe],
 })
 export class CartModule {}
